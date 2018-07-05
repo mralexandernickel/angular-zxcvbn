eb6a5b6ebc31c7207971f444c9026d1d67991c92
Alexander Nickel <mr.alexander.nickel@gmail.com>
2018-07-05 12:00:42 +0200

    Add first version of typed minStrength-validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NickelValidators } from './validators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public inputType = 'password';
+
+  public password: FormControl = new FormControl(this.password, [
+    NickelValidators.minStrength(2)
+  ]);
+
+  public showPassword(): void {
+    this.inputType = 'text';
+  }
+
+  public hidePassword(): void {
+    this.inputType = 'password';
+  }
 }
