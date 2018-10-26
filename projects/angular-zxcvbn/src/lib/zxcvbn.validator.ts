2d4bbb1430da43905b5eac3f8d8c2ac39dd103d4
Alexander Nickel <mr.alexander.nickel@gmail.com>
2018-10-26 12:22:58 +0200

    Rename minStrength to zxcvbn

--- projects/angular-zxcvbn/src/lib/zxcvbn.validator.ts
+++ projects/angular-zxcvbn/src/lib/zxcvbn.validator.ts
@@ -3,21 +3,18 @@ import * as zxcvbn_ from 'zxcvbn';
 
 const zxcvbn = zxcvbn_; // fix for rollup-error "cannot call namespace..."
 
-export interface IMinStrengthValidatorResult {
-  minStrength: zxcvbn_.ZXCVBNFeedback;
+export interface IZXCVBNValidatorResult {
+  zxcvbn: zxcvbn_.ZXCVBNFeedback;
 }
 
 export function zxcvbnValidator(minStrength: zxcvbn_.ZXCVBNScore): ValidatorFn {
-  return (control: AbstractControl): IMinStrengthValidatorResult | null => {
-    let response: IMinStrengthValidatorResult | null = null;
+  return (control: AbstractControl): IZXCVBNValidatorResult | null => {
+    let response: IZXCVBNValidatorResult | null = null;
     if (control.value && control.value.length > 0) {
       const result: zxcvbn_.ZXCVBNResult = zxcvbn(control.value);
       if (result.score < minStrength) {
         response = {
-          'minStrength': {
-            'warning': result.feedback.warning,
-            'suggestions': result.feedback.suggestions
-          }
+          'zxcvbn': result.feedback
         };
       }
     }
